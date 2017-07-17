cbd0af372f6b29d3f5ecbd16d932d4eb2d2c2585
Thiago Adriano <tadriano@band.com.br>
2017-07-17 18:43:56 -0300

    first commit

---
+++ src/App.ts
@@ -0,0 +1,45 @@
+//http://mherman.org/blog/2016/11/05/developing-a-restful-api-with-node-and-typescript/#.WWzjGIgrKM_
+
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+// Creates and configures an ExpressJS web server.
+class App {
+
+  // ref to Express instance
+  public express: express.Application;
+
+  //Run configuration methods on the Express instance.
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
+
+  // Configure Express middleware.
+  private middleware(): void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  // Configure API endpoints.
+  private routes(): void {
+    /* This is just to get up and running, and to make sure what we've got is
+     * working so far. This function will change when we start to add more
+     * API endpoints */
+    let router = express.Router();
+    // placeholder route handler
+    router.get('/', (req, res, next) => {
+      res.json({
+        message: 'Hello World!'
+      });
+    });
+    this.express.use('/', router);
+  }
+
+}
+
+export default new App().express;
